0b717ac76a6d217e29a864e930dfec996675bee8
Dan Stronge <dnlstronge@gmail.com>
2023-02-27 08:38:03 +0000

    [x] add drop downs for disability [x] add handlers

--- src/Components/Children/Children.js
+++ src/Components/Children/Children.js
@@ -7,6 +7,8 @@ export default function Children() {
     const handleChild = () => {
         hasChild ? setHasChild(false) : setHasChild(true)
     }
+    const handleLower = (e) => {}
+    const handleHigher = (e) => {}
 
   return (
     <div className={classes.container}>
@@ -14,7 +16,23 @@ export default function Children() {
             <input onClick={handleChild} className={classes.checkbox} id="children" type="checkbox"></input>
         </label>
         <label htmlFor="deps" className={classes.label}>Select Number of dependents
-            <select>
+            <select onchange={handleLower} id="deps" className={classes.select}>
+                <option value="0">Select</option>
+                <option value="0">0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+            </select>
+            </label>
+            <label htmlFor="dlaL" className={classes.label}>Disability lower
+            <select onChange={handleHigher} id="dlaL" className={classes.select}>
                 <option value="0">Select</option>
                 <option value="0">0</option>
                 <option value="1">1</option>
@@ -29,6 +47,7 @@ export default function Children() {
                 <option value="10">10</option>
             </select>
         </label>
+        
     </div>
   )
 };
