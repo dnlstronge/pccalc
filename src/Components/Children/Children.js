aece2e0601bc583d78dfd6d80b347f8bda1b217b
Dan Stronge <dnlstronge@gmail.com>
2023-02-27 08:31:34 +0000

    [x] - add handler for chiild checkbox

--- src/Components/Children/Children.js
+++ src/Components/Children/Children.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from "./Children.module.css";
 
 export default function Children() {
+    const [hasChild, setHasChild] = useState(false)
+
+    const handleChild = () => {
+        hasChild ? setHasChild(false) : setHasChild(true)
+    }
+
   return (
     <div className={classes.container}>
-        <label className={classes.label} htmlFor='children'>
-            <input className={classes.checkbox} id="children" type="checkbox"></input>
+        <label className={classes.label} htmlFor='children'>The claim includes dependents
+            <input onClick={handleChild} className={classes.checkbox} id="children" type="checkbox"></input>
         </label>
     </div>
   )
-}
+};
