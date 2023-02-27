5543852342190d20ba930c46d7d7dca3ccfa3bf7
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-02-27 10:51:03 +0000

    Update PensionCredit.js

--- src/Components/logic/PensionCredit.js
+++ src/Components/logic/PensionCredit.js
@@ -1,1 +1,26 @@
 
+/*===========Penion Credit Logic & calculation==============*/
+
+const thresholdSingle = 158.47
+const thresholdCouple = 251.70
+
+/* Args to be passed as state below*/
+
+const stateOBJ = {
+    applicableAmount: "0",
+    income: "0",
+    stateCouple: false
+  }
+
+const CalcPensionCredit = (applicableAmount, income, stateCOUPLE) => {
+  
+  // determines GPC 
+    let a = applicableAmount
+    let b = income
+    let c = stateCOUPLE
+    let GPC = 0
+    if ( a - b > 0) { GPC = a - b }
+  // determine SC
+    if ( b < 
+  
+    }
