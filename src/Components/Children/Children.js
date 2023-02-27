58f2e8ccd249f243f2374a58dd2f2df2f3a44783
Dan Stronge <dnlstronge@gmail.com>
2023-02-27 08:34:57 +0000

    [x] - add dropdown for children/deps

--- src/Components/Children/Children.js
+++ src/Components/Children/Children.js
@@ -13,6 +13,22 @@ export default function Children() {
         <label className={classes.label} htmlFor='children'>The claim includes dependents
             <input onClick={handleChild} className={classes.checkbox} id="children" type="checkbox"></input>
         </label>
+        <label htmlFor="deps" className={classes.label}>Select Number of dependents
+            <select>
+                <option value="0">Select</option>
+                <option value="0">0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+            </select>
+        </label>
     </div>
   )
 };
