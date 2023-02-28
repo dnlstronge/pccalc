02fb690c68aa793da3f4f93d5dab518555f10a1c
Dan Stronge <dnlstronge@gmail.com>
2023-02-28 08:41:09 +0000

    [x] - hook up handlers and assign values

--- src/Components/PA/Additional.js
+++ src/Components/PA/Additional.js
@@ -3,6 +3,8 @@ import classes from "./Additional.module.css"
 
 export default function Additional({ updateAction }) {
 
+   
+
     const handleSDP = (e) => {
 
     }
@@ -10,28 +12,28 @@ export default function Additional({ updateAction }) {
 
     }
     const handleTransitional = (e) => {
-        
+
     }
   return (
     <div className={classes.container}>
         <h5>Additional Amounts</h5>
         <label className={classes.label} htmlFor='sdp'>Severe Disabilty
-            <select className={classes.select} id="sdp">
-                <option>--select</option>
-                <option>One</option>
-                <option>Both</option>
-                <option>None</option>
+            <select onChange={handleSDP} className={classes.select} id="sdp">
+                <option value="SDPNULL">--select</option>
+                <option value="SDPONE">One</option>
+                <option value="SDPBOTH">Both</option>
+                <option calue="SDPNULL">None</option>
             </select>
         </label>
         <label className={classes.label} htmlFor='care'>Carer
-            <select className={classes.select} id="care">
-                <option>--select</option>
-                <option>One</option>
-                <option>Both</option>
-                <option>None</option>
+            <select onChange={handleCarer} className={classes.select} id="care">
+                <option value="CARENULL">--select</option>
+                <option value="CAREONE">One</option>
+                <option value="CAREBOTH">Both</option>
+                <option value="CARENULL">None</option>
             </select>
         </label>
-        <label className={classes.label} htmlFor='trans'>Transitional Amount (pw)
+        <label onChange={handleTransitional} className={classes.label} htmlFor='trans'>Transitional Amount (pw)
             <input className={classes.input} type="number" id="trans" />
         </label>
     </div>
