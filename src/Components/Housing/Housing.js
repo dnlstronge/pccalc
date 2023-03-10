2c6c7eb762673c3cc003d774c256cf2a37d9b926
Dan Stronge <dnlstronge@gmail.com>
2023-03-10 10:01:23 +0000

    [x] - add logic for savings credit threshold

--- src/Components/Housing/Housing.js
+++ src/Components/Housing/Housing.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import classes from "./Housing.module.css"
 
-export default function Housing() {
+export default function Housing({ updateAction }) {
 
   const handleHousing = (e) => {
-    
+    if(e.target.value > 0) {
+    updateAction({type: "HOUSING", housing: Number(e.target.value)})
+    }
   }
   return (
     <div className={classes.container}>
         <label className={classes.label} htmlFor='eligibleHousing'>Weekly eligible housing
-            <input className={classes.input} id="eligibleHousing" type="number"></input>
+            <input onChange={handleHousing} className={classes.input} id="eligibleHousing" type="number"></input>
         </label>
     </div>
   )
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,15 +1,16 @@
-import React, { Fragment, useReducer } from "react";
+import React, { Fragment, useReducer, useEffect } from "react";
 import Housing from "../Housing/Housing";
 import Additional from "../PA/Additional";
 import Children from "../Children/Children";
 import PA from "../PA/PA";
 import classes from "./Main.module.css";
+import elements from "../ApplicableAmounts/AA"
 
 const PC_REDUCER = (state, action) => {
   switch (action.type) {
     /* Eligible Housing */
     case "HOUSING": {
-      return {...state, housing: action.housing};
+      return { ...state, housing: action.housing };
     }
 
     /* Additonal Amounts */
@@ -19,13 +20,13 @@ const PC_REDUCER = (state, action) => {
 
     /* Personal Amounts */
     case "SELECT": {
-      return { ...state, stateCouple: false };
+      return { ...state, stateCouple: "", couple_value: 0, savingsCredit: 0 };
     }
     case "SINGLE": {
-      return { ...state, stateCouple: false };
+      return { ...state, stateCouple: "SINGLE", couple_value: elements.GPCsingle };
     }
     case "COUPLE": {
-      return { ...state, stateCouple: true };
+      return { ...state, stateCouple: "COUPLE", couple_value: elements.GPCcouple };
     }
     case "NONE": {
       return { ...state, claimType: "" };
@@ -33,8 +34,20 @@ const PC_REDUCER = (state, action) => {
     case "GPC": {
       return { ...state, claimType: "GPC" };
     }
-    case "GPSC": {
+    case "GPCSC": {
       return { ...state, claimType: "GPCSC" };
+    }
+
+    /* savings credit thresholds */
+
+    case "SCSINGLE": {
+      return {...state, savingsCredit: action.payload}
+    }
+    case "SCCOUPLE": {
+      return {...state, savingsCredit: action.payload}
+    }
+    case "SCNONE": {
+      return {...state, savingsCredit: 0}
     }
     default:
   }
@@ -45,16 +58,35 @@ const Main = (props) => {
     applicableAmount: 0,
     income: 0,
     additional: 0,
-    stateCouple: false,
+    couple_value: 0,
+    savingsCredit: 0,
+    stateCouple: "",
     claimType: "",
+    housing: 0,
   });
+
+  useEffect(() => {
+    if(applicable.claimType === "GPCSC" && applicable.stateCouple === "SINGLE") {
+      dispatchReducer({type: "SCSINGLE", payload: elements.SCSingle})
+    }
+    if(applicable.claimType === "GPCSC" && applicable.stateCouple === "COUPLE") {
+      dispatchReducer({type: "SCCOUPLE", payload: elements.SCCouple})
+    }
+    if(applicable.claimType === "GPC") {
+      dispatchReducer({type: "SCNONE"})
+    }
+
+    
+  }, [applicable.stateCouple, applicable.claimType])
+
+  
   return (
     <Fragment>
       <div className={classes.container}>
         <section className={classes.section}>
           <PA updateAction={dispatchReducer} />
           <Additional updateAction={dispatchReducer} />
-          <Housing updateAction={dispatchReducer}/>
+          <Housing updateAction={dispatchReducer} />
           <Children />
         </section>
 
@@ -62,15 +94,19 @@ const Main = (props) => {
 
         <section className={classes.statecontainer}>
           <p className={classes.state_p}>
-            Applicable Amount: {applicable.applicableAmount}{" "}
+            Personal amount: {applicable.couple_value}
           </p>
           <p className={classes.state_p}>Income: {applicable.income}</p>
           <p className={classes.state_p}>
             Is couple: {applicable.stateCouple ? "true" : "false"}
           </p>
           <p className={classes.state_p}>Claim Type: {applicable.claimType}</p>
-          <p className={classes.stae_p}>
+          <p className={classes.state_p}>Savings Credit Threshold: {applicable.savingsCredit}</p>
+          <p className={classes.state_p}>
             Additional Amounts: {applicable.additional.toFixed(2)}
+          </p>
+          <p className={classes.state_p}>
+            Eligible Housing: {applicable.housing}
           </p>
         </section>
       </div>
