795d758172b24d89a24f6d8f5a4cc46fab2a2648
Dan Stronge <dnlstronge@gmail.com>
2023-03-10 10:42:11 +0000

    [x] - add state for number of children

--- src/Components/Children/Children.js
+++ src/Components/Children/Children.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from "./Children.module.css";
 
 export default function Children() {
     const [hasChild, setHasChild] = useState(false)
     const [pre, setPre] = useState(false)
+    const [number, setNumber] = useState(0)
 
     const handleChild = () => {
         hasChild ? setHasChild(false) : setHasChild(true)
@@ -11,13 +12,23 @@ export default function Children() {
     const handlePre = () => {
         pre ? setPre(false) : setPre(true)
     }
+    const handleNumber = (e) => {
+        if(e.target.value > 0)
+        setNumber(Number(e.target.value))
+    }
 
-    
     const handleLower = (e) => {}
     const handleHigher = (e) => {}
 
+    /*Conditional styling */
+
     const conditionalStyle = hasChild ? classes.label : classes.label_disabled
     const conditionalBox = hasChild ? classes.label_checkbox : classes.label_checkbox_disabled
+
+
+    /* push to state:  */
+
+    useEffect(() => {}, [])
   return (
     <div className={classes.container}>
       <h5 className={classes.heading}>Dependents</h5>
@@ -28,7 +39,7 @@ export default function Children() {
             <input disabled={!hasChild} onClick={handlePre} className={classes.checkbox} id="children" type="checkbox"></input>
         </label>
         <label htmlFor="deps" className={conditionalStyle}>Number of dependents
-            <select disabled={!hasChild} onChange={handleChild} id="deps" className={classes.select}>
+            <select disabled={!hasChild} onChange={handleNumber} id="deps" className={classes.select}>
                 <option value="0">Select</option>
                 <option value="0">0</option>
                 <option value="1">1</option>
