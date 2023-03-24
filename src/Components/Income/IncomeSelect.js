8d36db3110dc0bea7451b06ada0bebc1ebddba52
Dan Stronge <dnlstronge@gmail.com>
2023-03-24 08:33:45 +0000

    [x] - add useEffect

--- src/Components/Income/IncomeSelect.js
+++ src/Components/Income/IncomeSelect.js
@@ -1,70 +1,91 @@
-import React, {useState, useEffect } from "react";
-import classes from "./IncomeSelect.module.css"
+import React, { useState, useEffect } from "react";
+import classes from "./IncomeSelect.module.css";
 
-const IncomeSelect = ({id, localState, updateState}) => {
-    const [incomeLocal, setIncomeLocal] = useState({
-        value: 0,
-        freq: "",
-        actual: 0
-    })
+const IncomeSelect = ({ id, localState, updateState }) => {
+  const [incomeLocal, setIncomeLocal] = useState({
+    value: 0,
+    freq: "",
+    actual: 0,
+  });
 
-    const handleAmount = (e) => {
-        setIncomeLocal({...incomeLocal, value: Number(e.target.value)})
+  const handleAmount = (e) => {
+    setIncomeLocal({ ...incomeLocal, value: Number(e.target.value) });
+  };
+
+  const handleFreq = (e) => {
+    setIncomeLocal({ ...incomeLocal, freq: e.target.value });
+  };
+
+  useEffect(() => {
+    let a = incomeLocal.value;
+    let b = incomeLocal.freq;
+    if (b === "W") {
+      setIncomeLocal({ ...incomeLocal, actual: a });
     }
-
-    const handleFreq = (e) => {
-        setIncomeLocal({...incomeLocal, freq: e.target.value})
+    if (b === "M") {
+      setIncomeLocal({ ...incomeLocal, actual: (a * 12) / 52 });
     }
+    if (b === "Q") {
+      setIncomeLocal({ ...incomeLocal, actual: (a * 4) / 52 });
+    }
+    if (b === "Y") {
+      setIncomeLocal({ ...incomeLocal, actual: a / 52 });
+    }
+  }, [
+    incomeLocal.value,
+    incomeLocal.freq,
+    incomeLocal.actual,
+    setIncomeLocal,
+    incomeLocal,
+  ]);
 
-    useEffect(() => {
-        let a = incomeLocal.value
-        let b = incomeLocal.freq
-        if(b === "W") {
-            setIncomeLocal({...incomeLocal, actual: a})
-        }
-        if(b === "M") {
-            setIncomeLocal({...incomeLocal, actual: (a * 12) / 52})
-        }
-        if(b === "Q") {
-            setIncomeLocal({...incomeLocal, actual: (a * 4) / 52})
-        }
-        if(b === "Y") {
-            setIncomeLocal({...incomeLocal, actual: a / 52})
-        }
+  // lift to state:
 
-        
-    }, [incomeLocal.value, incomeLocal.freq, incomeLocal.actual, setIncomeLocal, incomeLocal])
+  useEffect(() => {
+    if (incomeLocal.actual > 0) {
+    }
+  }, [incomeLocal.actual, id, localState, updateState]);
 
-
-
-    return (
-        <React.Fragment>
-            <div className={classes.container}>
-            <label className={classes.label} htmlFor="selectselect"> Enter Income
-                <select className={classes.select} id="selectselect">
-                    <option value="--select--"></option>
-                    <option value="State Pension"></option>
-                    <option value="State Pension (partner)"></option>
-                    <option value="Occupational Pension"></option>
-                    <option value="Occupational Pension (partner)"></option>
-                    <option value="Private Pension"></option>
-                    <option value="Private Pension (partner)"></option>
-                    <option value="Wages"></option>
-                    <option value="Wages (partner)"></option>
-                    <option value="Other"></option>
-                </select>
-                <input  onChange={handleAmount} className={classes.input} type="number"></input>
-                <select onChange={handleFreq}>
-                    <option value="N">--select</option>
-                    <option value="W">Weekly</option>
-                    <option value="M">Monthly</option>
-                    <option value="Q">Quarterly</option>
-                    <option value="Y">Yearly</option>
-                   
-                </select>
-                </label>
-            </div>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <div className={classes.container}>
+        <label className={classes.label} htmlFor="selectselect">
+          {" "}
+          Enter Income
+          <select className={classes.select} id="selectselect">
+            <option value="--select--">--select--</option>
+            <option value="State Pension">State Pension</option>
+            <option value="State Pension (partner)">
+              State Pension (partner)
+            </option>
+            <option value="Occupational Pension">Occupational Pension</option>
+            <option value="Occupational Pension (partner)">
+              Occupational pension (partner)
+            </option>
+            <option value="Private Pension">Private Pension</option>
+            <option value="Private Pension (partner)">
+              Private Pension (partner)
+            </option>
+            <option value="Wages">Wages</option>
+            <option value="Wages (partner)">Wages (partner)</option>
+            <option value="Other">Other</option>
+          </select>
+          <input
+            onChange={handleAmount}
+            className={classes.input}
+            type="number"
+          ></input>
+          <select onChange={handleFreq}>
+            <option value="N">--select</option>
+            <option value="W">Weekly</option>
+            <option value="M">Monthly</option>
+            <option value="Q">Quarterly</option>
+            <option value="Y">Yearly</option>
+          </select>
+        </label>
+        <div>TESTING: Actual = {incomeLocal.actual.toFixed(2)}</div>
+      </div>
+    </React.Fragment>
+  );
+};
 export default IncomeSelect;
