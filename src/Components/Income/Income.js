860e6fda28bc2d7d86c23dcff2e9baec3315a28e
Dan Stronge <dnlstronge@gmail.com>
2023-03-27 09:49:45 +0100

    [x] - add additional income Components [x] - hook these up to logic

--- src/Components/Income/Income.js
+++ src/Components/Income/Income.js
@@ -13,12 +13,12 @@ const Income = ({ updateAction }) => {
   const [val_1, setVal_1] = useState(0);
   const [val_2, setVal_2] = useState(0);
   const [val_3, setVal_3] = useState(0);
-  // const [val_4, setVal_4] = useState(0);
-  // const [val_5, setVal_5] = useState(0);
-  // const [val_6, setVal_6] = useState(0);
-  // const [val_7, setVal_7] = useState(0);
-  // const [val_8, setVal_8] = useState(0);
-  // const [val_9, setVal_9] = useState(0);
+  const [val_4, setVal_4] = useState(0);
+  const [val_5, setVal_5] = useState(0);
+  const [val_6, setVal_6] = useState(0);
+  const [val_7, setVal_7] = useState(0);
+  const [val_8, setVal_8] = useState(0);
+  const [val_9, setVal_9] = useState(0);
 
  
 
@@ -28,8 +28,14 @@ const Income = ({ updateAction }) => {
     let a = val_1
     let b = val_2
     let c = val_3
+    let d = val_4
+    let e = val_5
+    let f = val_6
+    let g = val_7
+    let h = val_8
+    let i = val_9
     let DR = local.DR
-    let SUM = a + b + c - DR
+    let SUM = a + b + c +d +e +f +g +h +i - DR
      setTOTAL(SUM)},
     [val_1, val_2, val_3, local.DR,  setTOTAL]
   );
@@ -68,6 +74,12 @@ const Income = ({ updateAction }) => {
         <IncomeSelect updateState={setVal_1} />
         <IncomeSelect updateState={setVal_2} />
         <IncomeSelect updateState={setVal_3} />
+        <IncomeSelect updateState={setVal_4} />
+        <IncomeSelect updateState={setVal_5} />
+        <IncomeSelect updateState={setVal_6} />
+        <IncomeSelect updateState={setVal_7} />
+        <IncomeSelect updateState={setVal_8} />
+        <IncomeSelect updateState={setVal_9} />
       </div>
     </React.Fragment>
   );
